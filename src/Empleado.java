c18f643d6eddcdb0abcf2fb800bd7f321a926a94
guillermoroman <guillermoroman@outlook.com>
2024-02-22 15:40:47 +0100

    first commit

---
+++ src/Empleado.java
@@ -0,0 +1,61 @@
+
+public class Empleado {
+    // Atributos de la clase
+    private String nombre;
+    private String id;
+    private double sueldo;
+
+
+
+    public Empleado(String nombre, String id, double sueldo) {
+        this.nombre = nombre;
+        this.id = id;
+        this.sueldo = sueldo;
+    }
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+    public String getId() {
+        return id;
+    }
+
+
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+
+
+    public void incrementarSueldo(double incremento) {
+        this.sueldo += incremento;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "nombre='" + nombre + '\'' +
+                ", id='" + id + '\'' +
+                ", sueldo=" + sueldo +
+                '}';
+    }
+}
---
+++ src/GestorPersonal.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GestorPersonal {
+
+
+    private List<Empleado> empleados;
+
+
+    public GestorPersonal() {
+        empleados = new ArrayList<>();
+    }
+
+
+    public void contratar(Empleado empleado) {
+        empleados.add(empleado);
+    }
+
+
+    public boolean despedir(String id) {
+        return empleados.removeIf(e -> e.getId().equals(id));
+    }
+
+
+    public boolean cambiarSueldo(String id, double nuevoSueldo) {
+        for (Empleado empleado : empleados) {
+            if (empleado.getId().equals(id)) {
+                empleado.setSueldo(nuevoSueldo);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public List<Empleado> getEmpleados() {
+        return new ArrayList<>(empleados);
+    }
+}
